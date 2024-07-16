51fc9eac0c034509687f3e408ed84dc34cc8b93a
jankatgurbuz <jankatgurbuz@gmail.com>
2024-07-16 11:52:23 +0300

    Deprecate unity canvas system for MainMenu

--- Assets/Scripts/MenuScene/Installer/MenuInstaller.cs
+++ Assets/Scripts/MenuScene/Installer/MenuInstaller.cs
@@ -10,19 +10,13 @@ namespace MenuScene.Installer
 {
     public class MenuInstaller : MonoInstaller<MenuInstaller>
     {
-        // [SerializeField] private NavigationView _navigationView;
-        // [SerializeField] private MenuPanelsHandler _menuPanelsHandler;
         [SerializeField] private List<NavigationCanvasView> navigationCanvasView;
 
         public override void InstallBindings()
         {
-            // Container.BindInterfacesTo<NavigationPanelController>().AsSingle().WithArguments(_navigationView).NonLazy();
-            //
-            // Container.BindInterfacesTo<MenuPanelController>().AsSingle().WithArguments(_menuPanelsHandler).NonLazy();
-
             Container.BindInterfacesTo<NavigationCanvasController>().AsSingle().WithArguments(navigationCanvasView)
                 .NonLazy();
-
+            
             // todo:  It is bad practice to call Inject/Resolve/Instantiate before all the Installers have completed!
             Container.Resolve<GameController>().LoadNextScene().Forget();
         }
